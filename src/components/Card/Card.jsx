f69bae8317c825c0ddf49ccd1b9253b370136c8e
khalidhersi <cipherpolfoundation@gmail.com>
2021-12-06 15:21:44 +0000

    starting props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const Card = () => {
+    const [ name, tagline, firstBrewe, imageUrl ] = props;
 
-    return <>
-
-    </>
+    return <div className="beer-container">
+        <img src={imageUrl} alt="" className="beer-image" />
+        <div className="beer-container__info">
+            <h3 className="beer-container__name"></h3>
+            <p className="beer-container__firstBrewe"></p>
+            <p className="beer-container__tagline"></p>
+        </div>
+    </div>
 
 }
 
--- src/containers/CardList/CardList.jsx
+++ src/containers/CardList/CardList.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import beers from "../../data/beers";
 
 const CardList = () => {
 
-    return <>
+    const [cardArr, setCardArr] = useState([]);
 
-    </>
+    const beersJSX = beers.map(beer => {
+        return <Card name={beer.name} tagline={beer.tagline} firstBrewe={beer.first_brewed} imageUrl={beer.image_url}/>
+    })
+
+    return <div>{beersJSX}</div>
 
 }
 
